5a36b346d590f2861642126357e97176396d4858
Hdsgf <zubelevichivan@gmail.com>
2024-03-27 12:46:28 +0300

    first commit

---
+++ server/src/todos/todo.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from "@nestjs/common";
+import {InjectModel} from "@nestjs/sequelize";
+import {Todo} from "./models/todo.model";
+import {CreateTodoDto} from "./dto/create-todo.dto";
+
+@Injectable()
+export class TodoService {
+    constructor(@InjectModel(Todo) private todoModel: typeof Todo) {}
+
+    async findAll(): Promise<Todo[]> {
+        return this.todoModel.findAll();
+    }
+
+    findOne(id: string): Promise<Todo> {
+        return this.todoModel.findOne({
+            where: {
+                id,
+            },
+        });
+    }
+
+    create(createTodo: CreateTodoDto): Promise<Todo> {
+        const todo = new Todo();
+
+        todo.title = createTodo.title;
+        todo.done = createTodo.done;
+
+        return todo.save();
+    }
+}
